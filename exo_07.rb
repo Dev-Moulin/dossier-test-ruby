07b9898e787fd71bea6d626b53e2313adc97d80a
Dev_Paul <p.moulin.95@gmail.com>
2024-09-30 22:04:38 +0200

    ajout exo_07.rb

---
+++ exo_07.rb
@@ -0,0 +1,5 @@
+puts "Bonjour, c'est quoi ton blase ?"
+user_name = gets.chomp                  
+#ici gets.chomp crée un input pour repondre a la question 
+#et l'imprime grace à "puts user_name"
+puts user_name
